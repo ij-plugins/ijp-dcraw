66d747ad7ef010a500700d842bdfa3ebd77a28ba
Jarek Sacha <jpsacha@gmail.com>
2021-08-13 14:54:44 -0400

    Update copyright and reformat
    Minimize future diffs due to formatting only

--- src/main/java/net/sf/ij_plugins/dcraw/DCRawException.java
+++ src/main/java/net/sf/ij_plugins/dcraw/DCRawException.java
@@ -1,7 +1,7 @@
 /*
- * Image/J Plugins
- * Copyright (C) 2002-2014 Jarek Sacha
- * Author's email: jsacha at users dot sourceforge dot net
+ * IJ-Plugins
+ * Copyright (C) 2002-2021 Jarek Sacha
+ * Author's email: jpsacha at gmail dot com
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -17,7 +17,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Latest release available at http://sourceforge.net/projects/ij-plugins/
+ * Latest release available at https://github.com/ij-plugins/ijp-toolkit/
  */
 package net.sf.ij_plugins.dcraw;
 
