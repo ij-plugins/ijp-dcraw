194e713315d059abc86d084b0557880476bd338e
Jarek Sacha <jpsacha@gmail.com>
2022-04-12 20:54:32 -0400

    Correct copyright notice

--- src/main/java/ij_plugins/dcraw/util/ExecProxy.java
+++ src/main/java/ij_plugins/dcraw/util/ExecProxy.java
@@ -1,6 +1,6 @@
 /*
- * IJ-Plugins
- * Copyright (C) 2002-2022 Jarek Sacha
+ * IJ-Plugins DCRaw
+ * Copyright (C) 2008-2022 Jarek Sacha
  * Author's email: jpsacha at gmail dot com
  *
  * This library is free software; you can redistribute it and/or
@@ -17,7 +17,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Latest release available at https://github.com/ij-plugins/ijp-toolkit/
+ * Latest release available at https://github.com/ij-plugins/ijp-dcraw
  */
 
 package ij_plugins.dcraw.util;
